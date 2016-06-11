e68bd215935972cf15ba79a0b257e49926786514
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2016-06-11 19:11:46 -0400

    cartpole random agent

--- cartpole/main.py
+++ cartpole/main.py
@@ -1,8 +1,34 @@
 import gym
+import time
 
-env = gym.make('CartPole-v0')
-env.reset()
+class RandomAgent():
+    def __init__(self, action_space):
+        self.action_space = action_space
+    def act(self, observation, reward, done):
+        return self.action_space.sample()
 
-for _ in xrange(1000):
-    env.render()
-    env.step(env.action_space.sample())
+def main():
+    env = gym.make('CartPole-v0')
+    #env.monitor.start('/tmp/cartpole-experiment-1', force=True, seed=0)
+
+    agent = RandomAgent(env.action_space)
+
+    episode_count = 100
+    max_steps = 200
+    reward = 0
+    done = False
+
+    for i_episode in range(episode_count):
+        ob = env.reset()
+        for t in range(max_steps):
+            action = agent.act(ob, reward, done)
+            ob, reward, done, _ = env.step(action)
+            print('reward:', reward)
+            if done:
+                break
+            time.sleep(1)
+
+    #env.monitor.close()
+
+if __name__ == '__main__':
+    main()

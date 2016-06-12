57b37059eced1cbb7ac0592df00e0747b7d95166
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2016-06-12 08:22:34 -0400

    add comment

--- cartpole/main.py
+++ cartpole/main.py
@@ -1,6 +1,7 @@
 import gym
 import time
 
+# class of the agent
 class RandomAgent():
     def __init__(self, action_space):
         self.action_space = action_space
@@ -8,19 +9,24 @@ class RandomAgent():
         return self.action_space.sample()
 
 def main():
+    # initialize the enironment
     env = gym.make('CartPole-v0')
     #env.monitor.start('/tmp/cartpole-experiment-1', force=True, seed=0)
 
+    # initialize agent
     agent = RandomAgent(env.action_space)
 
+    # set up run parameters
     episode_count = 100
     max_steps = 200
     reward = 0
     done = False
 
+    # run the episodes
     for i_episode in range(episode_count):
         ob = env.reset()
         for t in range(max_steps):
+            # get action from the agent
             action = agent.act(ob, reward, done)
             ob, reward, done, _ = env.step(action)
             print('reward:', reward)
